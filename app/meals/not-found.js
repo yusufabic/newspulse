f04dbbbdd39cb99e5f567e5d067d30e3c37f2b4e
YusufAbic <yusufabic@YusufAbic-MacBook-Air.local>
2024-08-25 13:41:08 +0300

    project setup created

---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Meal not Found</h1>
+      <p>Unfourtunately, we could not find the requested page or meal data.</p>
+    </main>
+  );
+}
