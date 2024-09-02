2a83205809a63a551e55dc69d7010e0064cc7f58
YusufAbic <yusuf.abic@outlook.com>
2024-09-02 11:23:15 +0300

    client and server components

--- app/components/main-header.js
+++ app/components/main-header.js
@@ -1,16 +1,22 @@
 import Link from "next/link";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
   return (
-    <header>
-      <ul id="main-header">
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/news">News</Link>
-        </li>
-      </ul>
+    <header id="main-header">
+      <div id="logo">
+        <Link href="/"> Next News </Link>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink href="/news">News</NavLink>
+          </li>
+          <li>
+            <NavLink href="/archive">Archive</NavLink>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
