ef236217cfdce8114928dfc7d906f019464c18cb
YusufAbic <yusuf.abic@outlook.com>
2024-09-05 11:16:20 +0300

    showed loading fallback

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -3,7 +3,7 @@ import NewsList from "../../components/news-list";
 import { getAllNews } from "@/lib/news";
 
 export default async function NewsPage() {
-  const news = getAllNews();
+  const news = await getAllNews();
 
   return (
     <>
